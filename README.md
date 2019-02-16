3442e9ef9370e10b4e59398dcf93846abc811c05
Przemyslaw Wieteska <wieteska.przemyslaw@gmail.com>
2019-02-16 17:47:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Asiafloralstudio-old
---
+++ eventy/kontakt.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=iso-8859-2">
+</head>
+<body>
+<?php if(isset($_POST['send']))
+{$to = 'arin@go2.pl';
+$subject = 'Wiadomoć ze strony';
+$imie = $_POST['osobakontaktowa'];
+$telefon = $_POST['telefon'];
+if(strlen($_POST['telefon'])>6){$telefon="(tel: $telefon)";}else{$telefon="";}
+$email = $_POST['email'];
+$wiadomosc = $_POST['wiadomosc'];
+$headers = "From: $email" . "\r\n" .
+"Reply-To: $email" . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($to, $subject, "Nowa wiadomoć od: $imie \n\n\n $wiadomosc", $headers);
+}
+echo "<b>Dziękujemy za wysłanie wiadomoci, proszę kliknšć WSTECZ aby wrócić do strony</b>";
+?>
+</body>
+</html>
